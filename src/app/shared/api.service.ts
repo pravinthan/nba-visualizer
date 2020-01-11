deac204c8b1c6599135cdea54343057d2597beef
pravinthan <pravinthan.prabagaran@gmail.com>
2020-01-11 14:06:49 -0500

    now using serverless-http npm package

--- src/app/shared/api.service.ts
+++ src/app/shared/api.service.ts
@@ -13,7 +13,7 @@ import { BoxScore } from "./box-score.model";
   providedIn: "root"
 })
 export class ApiService {
-  endpoint = "/api";
+  endpoint = "/.netlify/functions/api";
   headers = new HttpHeaders().set("Content-Type", "application/json");
 
   constructor(private http: HttpClient) {}
