0c2ffed61079b1b5a7226910d8cc4dd8186219d7
pravinthan <pravinthan.prabagaran@gmail.com>
2020-01-19 08:11:30 -0500

    play by play api fix

--- server/server.js
+++ server/server.js
@@ -7,11 +7,7 @@ const app = express();
 
 // Allow any method from any host and log requests
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
+  res.header("Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods", "OPTIONS, GET, POST, PUT, DELETE");
   next();
 });
