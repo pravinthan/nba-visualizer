63d189728e6a9e7c97cad6bf59812784d06d9927
pravinthan <pravinthan.prabagaran@gmail.com>
2020-01-20 02:38:52 -0500

    removed config var for api endpoint as it is meant for node only

--- src/app/shared/api.service.ts
+++ src/app/shared/api.service.ts
@@ -16,7 +16,8 @@ import { PlayByPlayVideo } from "./play-by-play-video.model";
   providedIn: "root"
 })
 export class ApiService {
-  endpoint = process.env.API_ENDPOINT || "http://localhost:4201/api";
+  endpoint = "/api";
+  // endpoint = "http://localhost:4201/api";
   headers = new HttpHeaders().set("Content-Type", "application/json");
 
   constructor(private http: HttpClient) {}
